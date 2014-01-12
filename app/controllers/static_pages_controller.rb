2880ec3b091786acbdb1cf0f06dce1dd6330c1e7
jjs88 <jjs457@gmail.com>
2014-01-12 00:04:20 -0500

    added user microposts feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,19 @@
 class StaticPagesController < ApplicationController
-  def home
-  	@micropost = current_user.microposts.build if signed_in?
-  end
-
-  def help
-  end
-
-  def about
-  end
   
-end
+
+	  def home
+	  	if signed_in?
+	  		@micropost = current_user.microposts.build 
+	  		@feed_items = current_user.feed.paginate(page: params[:page])
+	  	else 
+	  		@feed_items = [ ]
+	  		render 'static_pages/home'
+	  	end
+	  end
+
+	  def help
+	  end
+
+	  def about
+	  end
+  end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,10 @@ SampleApp::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
